7edaf331e69435bcf50c18bd9d2ffe07be905d5e
Aaron <aaronmcavinue@gmail.com>
2016-12-18 23:51:55 +0000

    Add react router.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
 require('babel-polyfill');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var router = require('react-router');
+var Router = router.Router;
+var Route = router.Route;
+var hashHistory = router.hashHistory;
+
+var ContactListContainer = require('./components/contact-list-container');
+
+var routes = (
+    <Router history={hashHistory}>
+        <Route path="/contacts" component={ContactListContainer} />
+    </Router>
+);
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(routes, document.getElementById('app'));
+});
 
